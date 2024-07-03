d861621b86cae95fce9fc8e56463a5e996841fe6
B0b0lin <123524177+B0b0lin@users.noreply.github.com>
2024-07-03 12:12:51 +0200

    Update Program.cs

--- Projekt_sklep_gui/Program.cs
+++ Projekt_sklep_gui/Program.cs
@@ -16,7 +16,7 @@ namespace Projekt_sklep_gui
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Sprzedaz());
+            Application.Run(new Magazyn());
         }
     }
 }
